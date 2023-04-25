0df72c3800d8b8087f40c1d53c8b09def4f3ec2f
Luboš Luňák <l.lunak@centrum.cz>
2023-04-25 20:58:14 +0200

    destructive tantrum targeting fixes
    https://forums.kleientertainment.com/klei-bug-tracker/oni/destructive-duplicants-may-target-invincible-buildings-such-as-rocket-engines-r40223/

---
+++ Source/Tantrum.cs
@@ -0,0 +1,25 @@
+using HarmonyLib;
+
+// https://forums.kleientertainment.com/klei-bug-tracker/oni/destructive-duplicants-may-target-invincible-buildings-such-as-rocket-engines-r40223/
+// Some building such as rocket engines are invincible, but they are also breakable.
+// Destructive dupes throwing a tantrum will target them and end up just standing
+// there, doing nothing.
+namespace FixesAndTweaks
+{
+    [HarmonyPatch(typeof(BuildingTemplates))]
+    public class BuildingTemplates_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(CreateRocketBuildingDef))]
+        public static void CreateRocketBuildingDef(BuildingDef def)
+        {
+            def.Breakable = false;
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(CreateMonumentBuildingDef))]
+        public static void CreateMonumentBuildingDef(BuildingDef def)
+        {
+            def.Breakable = false;
+        }
+    }
+}
