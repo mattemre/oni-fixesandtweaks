10e3c2e501a5a789036387fa8bded1bcb99cb238
Luboš Luňák <l.lunak@centrum.cz>
2023-04-26 21:05:27 +0200

    add PLib
    See README.PLib .

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,6 +1,14 @@
+using HarmonyLib;
+using PeterHan.PLib.Core;
+
 namespace FixesAndTweaks
 {
     public class Mod : KMod.UserMod2
     {
+        public override void OnLoad( Harmony harmony )
+        {
+            base.OnLoad( harmony );
+            PUtil.InitLibrary( false );
+        }
     }
 }
